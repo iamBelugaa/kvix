1bac160b541dacc6031f37415992c9c36e10c919
iamBelugaa <iamnilotpaldeka@gmail.com>
2025-06-29 14:13:53 +0530

    Init project, define databse config options and use functional option pattern to customize config.

---
+++ pkg/options/defaults.go
@@ -0,0 +1,61 @@
+package options
+
+import "time"
+
+const (
+	// Specifies the default base directory where IgniteDB will store its data files.
+	DefaultDataDir string = "/var/lib/ignitedb"
+
+	// Defines the default time duration between automatic compaction operations.
+	DefaultCompactInterval = time.Hour * 5
+
+	// Defines the max time duration between automatic compaction operations.
+	MaxCompactInterval = 168 * time.Hour
+
+	// Represents the minimum allowed size for a segment file in bytes (512MB).
+	MinSegmentSize uint64 = 512 * 1024 * 1024
+
+	// Represents the maximum allowed size for a segment file in bytes (4GB).
+	MaxSegmentSize uint64 = 4 * 1024 * 1024 * 1024
+
+	// Specifies the default target size for a new segment file in bytes (1GB).
+	DefaultSegmentSize uint64 = 1 * 1024 * 1024 * 1024
+
+	// Specifies the default subdirectory within the main data directory
+	// where segment files will be stored.
+	DefaultSegmentDirectory string = DefaultDataDir + "/segments"
+
+	// Defines the default prefix for segment file names.
+	// For example, a segment file might be named "segment-00001.db".
+	DefaultSegmentPrefix string = "segment"
+
+	// Maximum allowed size for a record key in bytes (64KB).
+	MaxKeySize uint16 = 65535
+
+	// Maximum allowed size for a record value in bytes (100MB).
+	MaxValueSize uint32 = 100 * 1024 * 1024
+
+	// Specifies the minimum supported version of IgniteDB required for compatibility.
+	MinSchemaVersion uint8 = 1
+
+	// Represents the current version of the IgniteDB format or schema.
+	CurrentSchemaVersion uint = 1
+
+	// Specifies the maximum supported version of IgniteDB.
+	MaxSchemaVersion uint8 = 255
+)
+
+// Holds the default configuration settings for an IgniteDB instance.
+var defaultOptions = Options{
+	DataDir:         DefaultDataDir,
+	CompactInterval: DefaultCompactInterval,
+	SegmentOptions: &SegmentOptions{
+		Size:      DefaultSegmentSize,
+		Prefix:    DefaultSegmentPrefix,
+		Directory: DefaultSegmentDirectory,
+	},
+}
+
+func DefaultOptions() Options {
+	return defaultOptions
+}
---
+++ pkg/options/options.go
@@ -0,0 +1,106 @@
+// Package options provides data structures and functions for configuring the Ignite database.
+package options
+
+import (
+	"strings"
+	"time"
+)
+
+// Defines configurable parameters for each segment.
+type SegmentOptions struct {
+	// Defines the maximum size a segment can grow to before rotation.
+	//
+	//  - Default: 1GB
+	//  - Maximum: 4GB
+	//  - Minimum: 512MB
+	Size uint64 `json:"maxSegmentSize"`
+
+	// Specifies where segment files are stored.
+	//
+	// Default: "/var/lib/ignitedb/segments"
+	Directory string `json:"directory"`
+
+	// Defines the filename prefix for segment files.
+	// Final filename will be: `prefix_segmentId_timestamp.seg`
+	//
+	// Default: "segment"
+	//
+	// Example: If Prefix is "mydata", a segment file might be "mydata_000001_20240525232100.seg".
+	Prefix string `json:"prefix"`
+}
+
+// Defines the configuration parameters for Ignite DB.
+type Options struct {
+	// Specifies the base path where files will be stored.
+	//
+	// Default: "/var/lib/ignitedb"
+	DataDir string `json:"dataDir"`
+
+	// Defines how often the compaction process runs to  merge old segments.
+	//
+	// Default: 5h
+	CompactInterval time.Duration `json:"compactInterval"`
+
+	// Configures segment management including size limits and naming convention.
+	SegmentOptions *SegmentOptions `json:"segmentOptions"`
+}
+
+type OptionFunc func(*Options)
+
+// Applies a predefined set of default configuration values to the Options struct.
+func WithDefaultOptions() OptionFunc {
+	return func(o *Options) {
+		opts := DefaultOptions()
+		o.DataDir = opts.DataDir
+		o.SegmentOptions = opts.SegmentOptions
+		o.CompactInterval = opts.CompactInterval
+	}
+}
+
+// Sets the primary data directory for Ignite.
+func WithDataDir(directory string) OptionFunc {
+	return func(o *Options) {
+		directory = strings.TrimSpace(directory)
+		if directory != "" {
+			o.DataDir = directory
+		}
+	}
+}
+
+// Sets the interval at which Ignite performs compaction operations.
+func WithCompactInterval(interval time.Duration) OptionFunc {
+	return func(o *Options) {
+		if interval > DefaultCompactInterval {
+			o.CompactInterval = interval
+		}
+	}
+}
+
+// Sets the directory specifically for storing segment files.
+func WithSegmentDir(directory string) OptionFunc {
+	return func(o *Options) {
+		directory = strings.TrimSpace(directory)
+		if directory != "" {
+			o.SegmentOptions.Directory = directory
+		}
+	}
+}
+
+// Sets the file name prefix for segment files.
+func WithSegmentPrefix(prefix string) OptionFunc {
+	return func(o *Options) {
+		prefix = strings.TrimSpace(prefix)
+		if prefix != "" {
+			o.SegmentOptions.Prefix = prefix
+		}
+	}
+}
+
+// Sets the maximum size of individual segment files.
+func WithSegmentSize(size uint64) OptionFunc {
+	return func(o *Options) {
+		if size > MinSegmentSize && size < MaxSegmentSize {
+			o.SegmentOptions.Size = size
+		}
+	}
+}
